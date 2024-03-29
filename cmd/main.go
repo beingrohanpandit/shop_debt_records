19acd74247628cd69b2bf9e9a0d941bd1f8d0d63
beingrohanpandit <rohansharma12434@gmail.com>
2024-03-29 13:38:10 +0530

    feat(main): Implement basic HTTP server for user management

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"net/http"
+
+	"example.com/internal/adapters/handler"
+	"example.com/internal/adapters/repository"
+	"example.com/internal/core/services"
+	"github.com/go-chi/chi"
+)
+
+var (
+	uvc *services.UserService
+)
+
+func main() {
+
+	r := chi.NewRouter()
+	store := repository.NewPostgresRepository()
+	uvc = services.NewUserService(store)
+	UserHandler := handler.NewHTTPUserHandler(*uvc)
+
+	r.Route("/user", func(r chi.Router) {
+		r.Get("/", UserHandler.GetUser)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
