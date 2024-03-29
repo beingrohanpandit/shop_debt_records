f21c2a4a29f4574f673157cfe7cdf90bb4343010
beingrohanpandit <rohansharma12434@gmail.com>
2024-03-29 14:35:13 +0530

    feat(Delete): Add URL Parameter in Delete User endpoint

---
+++ internal/core/domain/record_domain.go
@@ -0,0 +1,14 @@
+package domain
+
+type Record struct {
+	UserId         int              `json:"user_id"`
+	CreditAmount   float64          `json:"credit_amount"`
+	ProductDetails []ProductDetails `json:"product_details"`
+}
+
+type ProductDetails struct {
+	ProductId          int     `json:"product_id"`
+	ProductName        string  `json:"product_name"`
+	ProductDescription string  `json:"product_description"`
+	ProductPrice       float64 `json:"product_price"`
+}
