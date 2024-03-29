516f961dbe7f7fb00bf67b004b2bf69ce862e17d
beingrohanpandit <rohansharma12434@gmail.com>
2024-03-29 10:55:34 +0530

    feat(repository): Implement database connection

--- internal/adapters/repository/postgres.go
+++ internal/adapters/repository/postgres.go
@@ -1,1 +1,44 @@
 package repository
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PostgresRepository struct {
+	db *gorm.DB
+}
+
+func NewPostgresRepository() *PostgresRepository {
+	host := os.Getenv("host")
+	port := os.Getenv("port")
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	dbname := os.Getenv("dbname")
+
+	conn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		host,
+		port,
+		user,
+		dbname,
+		password)
+	db, err := gorm.Open(postgres.Open(conn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return &PostgresRepository{
+		db: db,
+	}
+}
+
+func (m *PostgresRepository) Close() {
+	db, err := m.db.DB()
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+}
