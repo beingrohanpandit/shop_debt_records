fa26f8b32a72fefbef09482acb78def186260fcb
beingrohanpandit <rohansharma12434@gmail.com>
2024-03-29 10:08:48 +0530

    feat(domain): Define user-related data structure

---
+++ internal/core/domain/user_domain.go
@@ -0,0 +1,16 @@
+package domain
+
+type GetUser struct {
+	UserId   int     `json:"user_id"`
+	FullName string  `json:"full_name"`
+	PhoneNo  string  `json:"phone_no"`
+	Address  string  `json:"address"`
+	Balance  float64 `json:"balance"`
+}
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"lasty_name"`
+	PhoneNo   string `json:"phone_no"`
+	Address   string `json:"address"`
+}
